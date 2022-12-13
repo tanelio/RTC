20c8415d8363db3cfdc660c90283bce6b483cd81
totala <taneli@otala.com>
2022-12-13 10:32:36 -0800

    Offending IP added

--- src/main/scala/rsyslog.scala
+++ src/main/scala/rsyslog.scala
@@ -8,11 +8,15 @@ import scala.language.postfixOps
 import RTC.RTC
 
 package rsyslog {
+
+  import scala.collection.mutable
+  import java.net.Inet4Address
+
   class Sensor extends Actor {
     val log = Logging(RTC.system, this)
     var InitialTimeout = 60
     context.setReceiveTimeout(InitialTimeout seconds)
-
+    val OffendingIP = new mutable.HashMap[Inet4Address, (Inet4Address, String)]()
     def receive = {
       case "text" => log.info("received text")
       case ReceiveTimeout => log.info("Timeout")
