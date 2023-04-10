83da4c0771344b9cd2da4e0ce6ee5fa6c95d7e81
totala <taneli@otala.com>
2023-04-10 13:42:55 -0700

    Start bulding Canvas/sheet

--- src/main/scala/Sensor/sensor.scala
+++ src/main/scala/Sensor/sensor.scala
@@ -15,21 +15,29 @@ class Historian(val len: Int, val t: Char ) {
 }
 */
 
+import java.sql.Timestamp
 import scala.collection.mutable.ArrayBuffer
 
-trait Pet {
-  var name: String
+object Historian {
+
+  trait Hist {
+    val name: String
+  }
+
+  class Hist2[A](val name: String) extends Hist {
+    val vals = Array[Timestamp, A, Char] // TS, Typ(Int,Long,Doube, String), Quality
+  }
+
+  class Dog(val name: String) extends Pet
+
+  val dog: Dog = new Dog("Harry")
+  val cat: Cat = new Cat("Sally")
+
+  val animals = ArrayBuffer.empty[Pet]
+  animals.append(dog)
+  animals.append(cat)
+  animals.foreach(pet => println(pet.name)) // Prints Harry Sally
+
+  println(animals(1).name)
+
 }
-
-class Cat(var name: String) extends Pet
-class Dog(var name: String) extends Pet
-
-val dog = new Dog("Harry")
-val cat = new Cat("Sally")
-
-val animals = ArrayBuffer.empty[Pet]
-animals.append(dog)
-animals.append(cat)
-animals.foreach(pet => println(pet.name))  // Prints Harry Sally
-
-println(animals(1).name)
